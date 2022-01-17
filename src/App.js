e325a099f8d5373ddd81d7fc42f47577ceded24e
jose zambrano <joseyzambranovpe@gmail.com>
2022-01-16 21:26:31 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Quote from "./Quote";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <section class="card container grid">
+        <h1>Random Quote</h1>
+        <div class="card_container">
+            <article class="card_content">
+
+
+             <h2 class="card_quote"> <Quote class="card_icon-90"/> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus at fusce cursus
+                    morbi lobortis nulla ultricies.
+                    Tincidunt viverra semper diam diam ame.</h2><Quote class="card_icon-rigth" />
+
+
+
+                <p class="card_author">
+                    Alexa Williams
+                </p>
+                <div class="card_button-content">
+                    <button class="card_button"><i class="uil uil-twitter"></i></button>
+                    <button class="card_button">Quote</button>
+                </div>
+                
+                
+            </article>
+        </div>
+    </section>
   );
 }
 
